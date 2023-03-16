4eb2354c95bb8b7e923e1ec6fdfd5c4d5d1a397a
Tewodros Abebe <teddyab729@gmail.com>
2023-03-16 15:13:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
       <h1 className="text-center font-bold text-2xl text-gray-700">
-        CRUD with redux toolkit
+        CRUD With Redux Toolkit
       </h1>
       <Routes>
         <Route path="/" element={<UserList />} />
